a5776bd88837f351fb3cb7f02ddb232e2dfe0fdb
Lisa-Kost <lisakost1804@gmail.com>
2024-10-07 06:01:34 -0400

    Added error handlers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -147,7 +147,7 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(len(data), 5)
 
-        def test_update_account(self):
+    def test_update_account(self):
         """It should Update an existing Account"""
         # create an Account to update
         test_account = AccountFactory()
@@ -163,7 +163,12 @@ class TestAccountService(TestCase):
         self.assertEqual(updated_account["name"], "Something Known")
 
         def test_delete_account(self):
-        """It should Delete an Account"""
-        account = self._create_accounts(1)[0]
-        resp = self.client.delete(f"{BASE_URL}/{account.id}")
-        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+            """It should Delete an Account"""
+            account = self._create_accounts(1)[0]
+            resp = self.client.delete(f"{BASE_URL}/{account.id}")
+            self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+
+        def test_method_not_allowed(self):
+            """It should not allow an illegal method call"""
+            resp = self.client.delete(BASE_URL)
+            self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
